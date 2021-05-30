c5a12deff3b4e9df231ef3b7d0e60a15068ec8b2
Mark W <markwuu@gmail.com>
2021-05-29 17:33:13 -0700

    Added links to pages on navigation panel

--- components/NavigationPanel.js
+++ components/NavigationPanel.js
@@ -1,25 +1,54 @@
 import styled from "styled-components";
+import Link from 'next/link';
 
 const NavigationContainer = styled.div`
   border: 1px solid black;
   width: 20vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Button = styled.button`
-  color: grey;
+const Panel = styled.div`
+  border: 1px solid black;
+  height: 10vh;
+`;
+
+const SettingsPanel = styled.div`
+  border: 1px solid black;
+  height: 10vh;
+  margin-top: auto;
 `;
 
 export default function NavigationPanel() {
   return (
     <NavigationContainer>
-      <Button>Hi</Button>
-        <div>Job Tracker</div>
-        <div>Dashboard</div>
-        <div>Algorithms</div>
-        <div>Jobs</div>
-        <div>Projects</div>
-        <div>Settings</div>
+        <Panel>Job Tracker</Panel>
+        <Panel>
+          <Link href="/">
+            <a>Dashboard</a>
+          </Link>
+        </Panel>
+        <Panel>
+          <Link href="/algorithm">
+            <a>Algorithms</a>
+          </Link>
+        </Panel>
+        <Panel>
+          <Link href="/job">
+            <a>Jobs</a>
+          </Link>
+        </Panel>
+        <Panel>
+          <Link href="/project">
+            <a>Projects</a>
+          </Link>
+        </Panel>
+        <SettingsPanel>
+          <Link href="/settings">
+            <a>Settings</a>
+          </Link>
+        </SettingsPanel>
     </NavigationContainer>
   )
 }
