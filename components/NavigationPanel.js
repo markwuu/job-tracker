30a43a7193743d7b6fdb2c1e22e5b950ab3fc981
Mark W <markwuu@gmail.com>
2021-05-29 17:20:19 -0700

    Added babelrc config to fix styling due to no render

--- components/NavigationPanel.js
+++ components/NavigationPanel.js
@@ -1,12 +1,25 @@
+import styled from "styled-components";
+
+const NavigationContainer = styled.div`
+  border: 1px solid black;
+  width: 20vw;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  color: grey;
+`;
+
 export default function NavigationPanel() {
   return (
-    <div style={{border: '1px solid black', width: '20vw', height: '100vh'}}>
+    <NavigationContainer>
+      <Button>Hi</Button>
         <div>Job Tracker</div>
         <div>Dashboard</div>
         <div>Algorithms</div>
         <div>Jobs</div>
         <div>Projects</div>
         <div>Settings</div>
-    </div>
+    </NavigationContainer>
   )
 }
