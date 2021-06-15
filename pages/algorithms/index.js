cf9921a4f84df4f476a57bf8b2c4f29f01c12856
Mark W <markwuu@gmail.com>
2021-06-14 20:48:26 -0700

    Displayed algorithm cards on algorithms page and connected post and get routes

--- pages/algorithms/index.js
+++ pages/algorithms/index.js
@@ -1,18 +1,151 @@
 import { useState, useEffect } from 'react';
 import { useSession } from "next-auth/client";
+import Link from "next/link";
 import Layout from '../../components/layout';
+import Form from '../../components/Form';
+import styled from 'styled-components';
+import { useRouter } from 'next/router'
+
+const PageContainer= styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const OuterTitleContainer = styled.div`
+  display: flex;
+  height: 80px;
+  margin: 0;
+  width: 900px;
+
+  @media (max-width: 1200px) {
+    width: 600px;
+  }
+`;
+
+const InnerTitleContainer = styled.div`
+  margin: auto 0;
+  font-size: 40px;
+  font-weight: 400;
+  width: 600px;
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  align-items: center;
+
+  h1 {
+    font-size: 34px;
+  }
+`;
+
+const AlgorithmsContainer= styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 900px;
+
+  @media (max-width: 1200px) {
+    width: 600px;
+  }
+`;
+
+const AlgorithmsListContainer= styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+`;
+
+const Algorithm = styled.div`
+  border-radius: 5px;
+  width: 600px;
+  color: #cad1dc;
+  background: #FFFFFF;
+  box-shadow: 0 3px 0px 0px;
+  height: 64px;
+  padding: 0px 20px;
+  margin: 0 0 25px 0;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+
+  a {
+    color: black;
+  }
+
+  p {
+    color: black;
+    font-size: 20px;
+  }
+
+  &:hover {
+      background: #e1e5ea;
+  }
+
+  @media (max-width: 890px) {
+    height: 100px;
+  }
+`;
+
+const ActivityLog = styled.div`
+  border-radius: 5px;
+  height: 435px;
+  width: 200px;
+  background: #FFFFFF;
+  border-radius: 5px;
+  margin: 0 0 30px 0;
+  color: #cad1dc;
+  box-shadow: 0 3px 0px 0px;
+  padding: 15px;
+  text-align: center;
+  font-size: 20px;
+
+  p {
+    color: black;
+  }
+
+  @media (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+const CreateButton = styled.button`
+  background: #ff9b7d;
+  color: white;
+  padding: 10px;
+  min-width: 100px;
+  text-align: center;
+  border-radius: 3.5px;
+  font-size: 12px;
+  letter-spacing: 0.2px;
+  border: none;
+  text-transform: uppercase;
+  cursor: pointer;
+  height: 35px;
+
+  &:hover {
+    background: #c37058;
+  }
+`;
 
 export default function Algorithms() {
   const [session, loading] = useSession();
-  const [content, setContent] = useState();
+  const [algorithms, setAlgorithms] = useState([]);
+  const [displayForm, setDisplayForm] = useState(false);
+  const [formNameValue, setFormNameValue] = useState('');
+  const [formDescriptionValue, setFormDescriptionValue] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("/api/algorithm");
       const json = await res.json();
+      console.log(json.data);
 
-      if(json.content) {
-        setContent(json.content);
+      if(json.data) {
+        const algorithms = json.data.map(({name, description, slugTitle}) => {
+          return { name, description, slugTitle };
+        });
+
+        setAlgorithms(algorithms);
       }
     }
 
@@ -23,20 +156,88 @@ export default function Algorithms() {
 
   if(!session) {
     return (
-      <main>
-        <div>
-          <h1>You arent signed in, please sign in first</h1>
-        </div>
-      </main>
+      <>
+        <h1>You arent signed in, please sign in first</h1>
+      </>
     )
+  }
+
+  const OpenCreateModal = () => {
+    document.body.style.height = '100%';
+    document.body.style.overflow = 'hidden';
+    setDisplayForm(true);
+  }
+
+  const CloseCreateModal = () => {
+    document.body.style.height = 'auto';
+    document.body.style.overflow = 'visible';
+    setDisplayForm(false);
+  }
+
+  const PostCreateAlgorithm = async () => {
+    const res = await fetch("/api/algorithm", {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: formNameValue,
+        description: formDescriptionValue
+      })
+    });
+
+    router.reload();
+  }
+
+  const onFormNameChange = (event) => {
+    setFormNameValue(event.target.value)
+  }
+
+  const onFormDescriptionChange = (event) => {
+    setFormDescriptionValue(event.target.value)
   }
 
   return (
     <Layout page={'algorithms'}>
-      <h1>Private Algorithms Overview Page</h1>
-      <p>
-        {content}
-      </p>
+      { displayForm ?
+        <Form
+          CloseCreateModal={CloseCreateModal}
+          PostCreate={PostCreateAlgorithm}
+          formNameValue={formNameValue}
+          formDescriptionValue={formDescriptionValue}
+          onFormNameChange={onFormNameChange}
+          onFormDescriptionChange={onFormDescriptionChange}
+          name={'Algorithm Name'}
+          description={'Description'}
+        />
+      : ''}
+      <PageContainer>
+        <OuterTitleContainer>
+          <InnerTitleContainer>
+            <h1>Algorithms</h1>
+            <CreateButton onClick={OpenCreateModal}>Create</CreateButton>
+          </InnerTitleContainer>
+        </OuterTitleContainer>
+        <AlgorithmsContainer>
+          <AlgorithmsListContainer>
+            {algorithms.map((algorithm, i) => {
+              const link = `/algorithm/${(algorithm.slugTitle).replace(" ","-").toLowerCase()}`;
+              return (
+                <Link href={link} style={{color: 'black'}}>
+                  <Algorithm key={i}>
+                    <p>{algorithm.name}</p>
+                    <p>{algorithm.description}</p>
+                  </Algorithm>
+                </Link>
+              )
+            })}
+          </AlgorithmsListContainer>
+          <ActivityLog>
+            <p>Activity Log</p>
+          </ActivityLog>
+        </AlgorithmsContainer>
+      </PageContainer>
     </Layout>
   )
 }
--- pages/api/algorithm/index.js
+++ pages/api/algorithm/index.js
@@ -1,15 +1,48 @@
 import { getSession } from "next-auth/client";
+import { connectToDatabase } from "../../../util/mongodb";
 
 export default async (req, res) => {
-    const session = await getSession({req});
-    const { userId } = session;
 
-    // Use user id to grab user model and algorithms data
+    if (req.method === 'GET') {
+        const session = await getSession({req});
+        if(session) {
+            const { userId } = session;
+            const { db } = await connectToDatabase();
 
+            //get algorithms
+            const algorithms = await db
+                .collection("algorithms")
+                .find({_user: userId})
+                .toArray();
 
-    if(session){
-        res.send({ content: "Algorithm overview data" });
-    } else {
-        res.send({ error: "You need to be signed in to view algorithm data" });
+            res.send({ data: algorithms.reverse() });
+        } else {
+            res.send({ error: "You need to be signed in to view job data" });
+        }
+    } else if (req.method === 'POST') {
+        const session = await getSession({req});
+
+        if(session) {
+            const { userId } = session;
+            const { db } = await connectToDatabase();
+            console.log('req.body', req.body);
+
+            // create job
+            const newAlgorithm = await db
+                .collection("algorithms")
+                .insertOne({
+                    _user: userId,
+                    name: req.body.name,
+                    slugTitle: req.body.name.replace(" ", "-"),
+                    description: req.body.description,
+                })
+                .then((res) => {
+                    console.log('res', res.ops[0]._id);
+                })
+
+            res.send({ data: null });
+        } else {
+            res.send({ error: "You need to be signed in to view project data" });
+        }
     }
 }
