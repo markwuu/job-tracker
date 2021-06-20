18ecd813c80a1814b2f233e6c878d0cda4677444
Mark W <markwuu@gmail.com>
2021-06-19 18:11:45 -0700

    Added project page title struture

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -2,6 +2,53 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import { useSession } from "next-auth/client";
 import Layout from '../../components/layout';
+import styled from 'styled-components';
+
+const PageContainer= styled.div`
+  border: 1px solid black;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const OuterTitleContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  height: 80px;
+  margin: 0;
+  width: 750px;
+
+  @media (max-width: 1200px) {
+    width: 600px;
+  }
+`;
+
+const InnerTitleContainer = styled.div`
+  border: 1px solid black;
+  margin: auto 0;
+  font-size: 40px;
+  font-weight: 400;
+  width: 750px;
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  align-items: center;
+
+  h1 {
+    font-size: 34px;
+  }
+`;
+
+const ProjectContainer= styled.div`
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  width: 750px;
+
+  @media (max-width: 1200px) {
+    width: 600px;
+  }
+`;
 
 const Project = () => {
   const router = useRouter()
@@ -34,14 +81,22 @@ const Project = () => {
   }
 
   return (
-    <Layout page={'projects'}>
-      <h1>{project && project.title ? project.title : `No project found with title ${id}`}</h1>
-      <p>{project && project.description ? project.description : ''}</p>
-      {
-        project && project.status ? (
-          <p>{project.status === 'incomplete' ? 'Not finished' : 'Finished'}</p>
-        ) : ''
-      }
+    <Layout page={null}>
+      <PageContainer>
+        <OuterTitleContainer>
+          <InnerTitleContainer>
+            <h1>{project && project.title ? project.title : ''}</h1>
+          </InnerTitleContainer>
+        </OuterTitleContainer>
+        <ProjectContainer>
+          <p>{project && project.description ? project.description : ''}</p>
+          {
+            project && project.status ? (
+              <p>{project.status === 'incomplete' ? 'Not finished' : 'Finished'}</p>
+            ) : ''
+          }
+        </ProjectContainer>
+      </PageContainer>
     </Layout>
   )
 }
