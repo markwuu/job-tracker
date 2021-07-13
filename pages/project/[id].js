fca39fb1bdd581dabe709243ad2572538ddcaf3a
Mark W <markwuu@gmail.com>
2021-07-12 20:01:09 -0700

    Styled form

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -5,7 +5,7 @@ import Layout from '../../components/layout';
 import styled from 'styled-components';
 
 const PageContainer= styled.div`
-  border: 1px solid black;
+  border: 0px solid black;
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -61,12 +61,73 @@ const ProjectContainer= styled.div`
 const Form = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid yellow;
-  min-height: 500px;
+  /* border: 1px solid yellow; */
   min-width: 250px;
+  min-width: 250px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 15px 35px 35px 35px;
+  box-shadow: 0 3px 0px 0px;
+  color: #cad1dc;
 
   label {
-    border: 1px solid black;
+    border: 0px solid black;
+    padding: 10px 0 2px 0;
+    color: black;
+    font-size: 18px;
+    text-transform: uppercase;
+  }
+
+  input {
+    border: 1px solid #898d94;
+    margin-bottom: 10px;
+    border-radius: 3px;
+    height: 20px;
+  }
+
+  input[type=submit] {
+    margin-top: 15px;
+    background: #9d99cb;
+    color: white;
+    padding: 10px;
+    min-width: 100px;
+    text-align: center;
+    border-radius: 3.5px;
+    font-size: 12px;
+    -webkit-letter-spacing: 0.2px;
+    -moz-letter-spacing: 0.2px;
+    -ms-letter-spacing: 0.2px;
+    letter-spacing: 0.2px;
+    border: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    height: 35px;
+
+    &:hover {
+      background: #817da6;
+    }
+  }
+
+  button {
+    background: #de6161;
+    color: white;
+    padding: 10px;
+    min-width: 100px;
+    text-align: center;
+    border-radius: 3.5px;
+    font-size: 12px;
+    -webkit-letter-spacing: 0.2px;
+    -moz-letter-spacing: 0.2px;
+    -ms-letter-spacing: 0.2px;
+    letter-spacing: 0.2px;
+    border: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    height: 35px;
+
+    &:hover {
+      background: #ae4f4f;
+    }
   }
 `;
 
@@ -191,9 +252,6 @@ const Project = () => {
             </Container>
           ) : (
             <Form>
-              <div>
-                Update project
-              </div>
               <label>
                   title
               </label>
